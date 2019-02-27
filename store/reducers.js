444ed4b44b0648cb22fcfc707759370873331eab
Shubham Bhardwaj <shubhard@publicisgroupe.net>
2019-02-27 18:48:16 +0530

    making favorite toggleable

--- store/reducers.js
+++ store/reducers.js
@@ -12,12 +12,10 @@ const AvengersList = (state = initialState, action) => {
        
         case actions.SET_FAVORITES:
         const isMemberExists = state.favorites.find((ITEM)=> ITEM.Name===action.item.Name);
-        if(!isMemberExists) {
-            action.item.isFavorite = !action.item.isFavorite
-            return {...state, favorites: [...state.favorites, action.item]}
-        } else {
-            return state;
-        }
+        console.log("reducing", action.item.isFavorite)
+        action.item.isFavorite = !action.item.isFavorite
+        console.log("reduced", action.item.isFavorite)
+        return {...state, favorites: [...state.favorites, action.item]}
 
         default:
         return state
